894a0c437a87cecaee040ffcaf07206923cad7c2
maoel <pedrohduarte007@gmail.com>
2026-07-24 01:15:49 -0300

    refactor: migrate file organizer script to TypeScript

--- organizar.ts
+++ organizar.ts
@@ -1,17 +1,16 @@
-const fs = require("fs");
-const os = require("os");
-const path = require("path");
+import fs from "fs";
+import os from "os";
+import path from "path";
 
 let usuario = os.homedir();
 
 let desktop = path.join(usuario, "Desktop");
 
-if(!fs.existsSync(desktop)){
-  desktop = path.join(usuario,"OneDrive","Desktop")
+if (!fs.existsSync(desktop)) {
+  desktop = path.join(usuario, "OneDrive", "Desktop");
 }
 
 const modelos3D = path.join(desktop, "Modelos 3D");
-console.log(modelos3D);
 
 if (!fs.existsSync(modelos3D)) {
   fs.mkdirSync(modelos3D);
@@ -23,6 +22,7 @@ const dir3mf = path.join(modelos3D, "3mf");
 if (!fs.existsSync(dirStl)) {
   fs.mkdirSync(dirStl);
 }
+
 if (!fs.existsSync(dir3mf)) {
   fs.mkdirSync(dir3mf);
 }
@@ -33,20 +33,21 @@ files.forEach((file) => {
   const ext = path.extname(file);
   const origem = path.join(desktop, file);
 
-  const info = fs.statSync(origem)
+  const info = fs.statSync(origem);
 
-  if(!info.isFile()){
-    return
+  if (!info.isFile()) {
+    return;
   }
-
 
   if (ext === ".stl") {
     const destino = path.join(dirStl, file);
     fs.renameSync(origem, destino);
   }
+
   if (ext === ".3mf") {
     const destino = path.join(dir3mf, file);
     fs.renameSync(origem, destino);
   }
+
   console.log(file, ext);
 });
